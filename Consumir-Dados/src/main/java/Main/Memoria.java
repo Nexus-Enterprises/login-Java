dc515976c89bb8becc74e91bd2e9d5c87a8d833e
zzz.miike <michael.silva@sptech.school>
2023-10-14 15:40:56 -0300

    Mais Algumas alteracoes na memoria

--- Consumir-Dados/src/main/java/Main/Memoria.java
+++ Consumir-Dados/src/main/java/Main/Memoria.java
@@ -7,11 +7,11 @@ public class Memoria {
     public String memoria() {
         Looca looca = new Looca();
         Double memEmUso = Double.valueOf(looca.getMemoria().getEmUso());
-        Double emUso = ((memEmUso / 1000) / 1000) / 1000;
+        Double emUso = ((memEmUso / 1024) / 1024) / 1024;
         Double memDisponivel = Double.valueOf(looca.getMemoria().getDisponivel());
-        Double disponivel = ((memDisponivel / 1000) / 1000) / 1000;
+        Double disponivel = ((memDisponivel / 1024) / 1024) / 1024;
         Double memTotal = Double.valueOf(looca.getMemoria().getTotal());
-        Double total = ((memTotal / 1000) / 1000) / 1000;
+        Double total = ((memTotal / 1024) / 1024) / 1024;
 
         return """
                 *-------------------------------------------------*         
